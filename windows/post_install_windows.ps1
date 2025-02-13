58aaaa67ce82a4c239ab94c20e7b3c8d07395db5
MerkeX <d.zanon93@gmail.com>
2025-02-13 15:44:15 +0100

    Updated 'Windows' script

--- windows/post_install_windows.ps1
+++ windows/post_install_windows.ps1
@@ -39,18 +39,17 @@ echo ":: We have installed chocolatey successfully. Now installing the gui..."; 
 choco.exe install chocolateygui -y; sleep 2
 echo ":: Installing various softwares..."; sleep 2
 # Delete software you don't need/want to install
-choco.exe install 7zip adobereader arduino audacity autohotkey blender `
-ccleaner  cmake cpu-z discord dosbox droidcamclient epicgameslauncher `
-electrum ffmpeg filezilla firefox gimp git gitkraken goggalaxy `
-google-drive-file-stream googlechrome gpu-z gsmartcontrol horizon hwinfo `
-imgburn itunes jdk11 libreoffice-fresh llftool keepassxc make memreduct `
+choco.exe install 7zip adobereader audacity autohotkey bleachbit `
+blender cmake cpu-z discord dosbox droidcamclient ea-app epicgameslauncher `
+electrum ffmpeg filezilla firefoxesr foxitreader gimp git gitkraken goggalaxy `
+googledrive googlechrome gpu-z gsmartcontrol horizon hwinfo `
+imgburn itunes jdk11 libreoffice-still llftool keepassxc make memreduct `
 microsoft-edge microsoft-teams microsoft-windows-terminal miktex `
 mp3gain-gui msiafterburner ninja nodejs notepadplusplus nvidia-geforce-now `
-onedrive openal origin parsec pcsx2 playnite python3 qbittorrent rainmeter `
-recuva rclone rocketdock revo-uninstaller signal skype spotify speccy `
+onedrive openal parsec pcsx2 playnite python3 qbittorrent rainmeter `
+recuva rclone rocketdock revo-uninstaller rufus signal skype spotify speccy `
 speedtest steam-client teamviewer telegram teracopy texstudio thunderbird `
-ubisoft-connect vlc vscode wget win32diskimager winfsp winrar `
-vmware-workstation-player vmware-tools whatsapp youtube-dl -y
+ubisoft-connect vlc vscodium wget winfsp winrar whatsapp yt-dlp -y
 echo ":: DONE. Now exiting..."
 $args
 Pause
